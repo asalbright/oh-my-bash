7450098a4aea33211c0b8c0c3fe18ab8a83f2198
Albright, Andrew 446434393 <andrew.s.albright@nasa.gov>
2024-09-24 09:10:57 -0500

    Added rosdep plugin

---
+++ custom/plugins/rosdep_custom/rosdep.plugin.sh
@@ -0,0 +1,7 @@
+# Add your own custom plugins in the custom/plugins directory. Plugins placed
+# here will override ones with the same name in the main plugins directory.
+
+# Aliases
+alias rosdepu="rosdep update"
+alias rosdepi="rosdep install --from-paths src/ -iry"
+alias rosdepui="rosdep update && rosdep install --from-paths src/ -iry"
