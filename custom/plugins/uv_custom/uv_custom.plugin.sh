67de5de19d5918f1fc98083b71d0a1c8171b3761
Andrew Albright <andrew.s.albright@nasa.gov>
2025-02-20 11:21:34 -0500

    added uv custom plugin

---
+++ custom/plugins/uv_custom/uv_custom.plugin.sh
@@ -0,0 +1,8 @@
+# Add your own custom plugins in the custom/plugins directory. Plugins placed
+# here will override ones with the same name in the main plugins directory.
+
+alias uva='source .venv/bin/activate'
+alias uvd='deactivate'
+
+alias uvs='uv sync'
+alias uvb='uv build'
