c2e98647bdeb41680d7db6fdba3298892bd3a9d7
Andrew Albright <andrew.s.albright@nasa.gov>
2025-04-24 14:33:08 -0400

    stuff

--- custom/plugins/docker_custom/docker_custom.plugin.sh
+++ custom/plugins/docker_custom/docker_custom.plugin.sh
@@ -6,3 +6,25 @@ fi
 if [ -f /entrypoint.bash ]; then
     source /entrypoint.bash
 fi
+
+# Function to get the list of docker compose services
+_get_docker_compose_services() {
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    local services
+    services=$(docker compose ps --services 2>/dev/null)
+    COMPREPLY=($(compgen -W "$services" -- "$cur"))
+}
+
+# Function to copy the .oh-my-bash and .bashrc to the container
+complete -F _get_docker_compose_services docker_compose_copy_omb
+docker_compose_copy_omb() {
+    # Get the HOME environment variable from the container
+    local home
+    home=$(docker compose exec "$1" printenv HOME)
+    # Copy .oh-my-bash
+    docker compose cp ~/.oh-my-bash "$1":${home}/.oh-my-bash
+    # Copy .bashrc
+    docker compose cp ~/.bashrc "$1":${home}/.bashrc
+}
+
+alias "docker-compose-copy-omb"="docker_compose_copy_omb"
--- custom/plugins/ros_custom/ros_custom.plugin.sh
+++ custom/plugins/ros_custom/ros_custom.plugin.sh
@@ -1,19 +1,21 @@
 ## Attempt to source a ros2 distro setup.bash
-ROS_DISTROS_DIR=($(ls /opt/ros/))
-# Try and source them in an order according to newest to oldest release
+ROS_DISTROS_DIR=/opt/ros/
 INSTALLED_DISTROS=("noetic")
-for distro in "${INSTALLED_DISTROS[@]}"
-do
-    if [[ " ${ROS_DISTROS_DIR[@]} " =~ " ${distro} " ]]; then
-        source /opt/ros/${distro}/setup.bash >/dev/null 2>&1
-    fi
-done 
 
-# Check to make sure ROS_DISTRO is set
-if [ -z "$ROS_DISTRO" ]; then
-    echo "ROS_DISTRO is not set. Cannot really make use of the ROS Custom Oh My Bash plugin ..."
+# Check if any of the distros are installed
+if [ ! -d "$ROS_DISTROS_DIR" ]; then
+    echo "ROS is not installed. Cannot really make use of the ROS Custom Oh My Bash plugin ..."
     return
 fi
+
+# Try and source them in an order according to newest to oldest release
+ROS_DISTROS_DIRS=($(ls $ROS_DISTROS_DIR))
+for distro in "${INSTALLED_DISTROS[@]}"
+do
+    if [[ " ${ROS_DISTROS_DIRS[@]} " =~ " ${distro} " ]]; then
+        source $ROS_DISTROS_DIR${distro}/setup.bash >/dev/null 2>&1
+    fi
+done 
 
 ## ROS Aliases
 alias sdsb="source devel/setup.bash"
